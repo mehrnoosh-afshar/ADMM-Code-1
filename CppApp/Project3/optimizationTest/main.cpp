b789cb89d2786490f4123e4f68d6b11cdb9be77c
mehrnoosh-afshar <56322879+mehrnoosh-afshar@users.noreply.github.com>
2020-12-03 16:04:05 -0700

    Update main.cpp

--- CppApp/Project3/optimizationTest/main.cpp
+++ CppApp/Project3/optimizationTest/main.cpp
@@ -1,9 +1,14 @@
 #include <Eigen/Core>
+#include <Eigen/Sparse>
 #include <iostream>
 #include <LBFGS.h>
 
+
 using Eigen::VectorXd;
 using namespace LBFGSpp;
+
+typedef Eigen::SparseMatrix<double> SpMat;
+typedef Eigen::Triplet<double> T;
 
 class Rosenbrock
 {
@@ -11,9 +16,11 @@ private:
     int n;
 public:
     Rosenbrock(int n_) : n(n_) {}
-    double operator()(const VectorXd& x, VectorXd& grad)
+    double operator()(const VectorXd& x, VectorXd& grad, Eigen::MatrixXd & Hessian)
     {
         double fx = 0.0;
+        //std::vector<T> coefficients; // list of non-zeros coefficients
+        Hessian.resize(n, n);
         for (int i = 0; i < n; i += 2)
         {
             double t1 = 1.0 - x[i];
@@ -21,7 +28,23 @@ public:
             grad[i + 1] = 20 * t2;
             grad[i] = -2.0 * (x[i] * grad[i + 1] + t1);
             fx += t1 * t1 + t2 * t2;
+            double hi_i = 2 - 400 * (x[i + 1] - 3 * x[i] * x[i]);
+            double hi_ii = -400 * x[i];
+            double hii_i = -400 * x[i];
+            double hii_ii = 200;
+           // coefficients.push_back(T(i, i, hi_i));
+           // coefficients.push_back(T(i, i + 1, hi_ii));
+           // coefficients.push_back(T(i + 1, i, hii_i));
+           // coefficients.push_back(T(i + 1, i + 1, hii_ii));
+
+            Hessian(i, i ) = hi_i;
+            Hessian(i, i + 1) = hi_ii;
+            Hessian(i+1, i + 1) = hii_i;
+            Hessian(i+1, i + 1) = hii_ii;
+
         }
+        //Hessian.resize(n, n);
+        //Hessian.setFromTriplets(coefficients.begin(), coefficients.end());
         return fx;
     }
 };
